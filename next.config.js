cb9f70479efde1eb6a3c22a2951131555175fec8
Hans Kristian Flaatten <hans@flaatten.org>
2023-10-27 06:38:23 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   publicRuntimeConfig: {
     title: 'Plattformpodden',
     tagline: 'En podcast om applikasjonsplattformer og teamene som bygger de.',
-    description: 'I denne podcasten snakker Hans Kristian Flaatten og Audun Strand med menneskene som bygger applikasjonsplattformer og dykker ned i hvordan plattformen er bygget opp og hvilke unike problemstillinger teamene må løse for sin organisasjon.',
+    description: 'I denne podcasten snakker Hans Kristian Flaatten og Audun Fauchald Strand med menneskene som bygger applikasjonsplattformer og dykker ned i hvordan plattformen er bygget opp og hvilke unike problemstillinger teamene må løse for sin organisasjon.',
     hosts: [
-      'Audun Strand',
+      'Audun Fauchald Strand',
       'Hans Kristian Flaatten',
     ]
   },
