4d03243dde560025d99a70307fa6cbdd42249859
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2023-10-27 10:31:01 +0200

    Move episode URL to /episode/[:number:]

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -72,7 +72,7 @@ function EpisodeEntry({ episode }: { episode: Episode }) {
               /
             </span>
             <Link
-              href={`/${episode.id}`}
+              href={`/episode/${episode.id}`}
               className="flex items-center text-sm font-bold leading-6 text-pink-500 hover:text-pink-700 active:text-pink-900"
               aria-label={`Vis notater for episode ${episode.title}`}
             >
