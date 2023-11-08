424511dfd091218b0495f7da420554b454ee5235
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2023-11-08 15:07:32 +0100

    Add links to Google Podcasts and Pocket Casts

--- next.config.js
+++ next.config.js
@@ -11,7 +11,9 @@ const nextConfig = {
     providerUrls: {
       'RSS Feed': 'https://feeds.transistor.fm/plattformpodden',
       'Spotify': 'https://open.spotify.com/show/10Cgw1ewV6QSQiH6JLNrQ5',
-      'Apple Podcast': 'https://podcasts.apple.com/no/podcast/plattformpodden/id1713936153',
+      'Apple Podcasts': 'https://podcasts.apple.com/no/podcast/plattformpodden/id1713936153',
+      'Google Podcasts': 'https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy50cmFuc2lzdG9yLmZtL3BsYXR0Zm9ybXBvZGRlbg==',
+      'Pocket Casts': 'https://pca.st/loez5z2p',
       'Overcast': 'https://overcast.fm/itunes1713936153',
     },
   },
